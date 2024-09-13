7089e85f3dd56c20a8b952b225f8f35d65a2ea12
Felipe Lima <pegasusyt1@gmail.com>
2024-09-13 16:42:48 -0300

    carrinho finalizado

--- pages/cart.js
+++ pages/cart.js
@@ -1,59 +1,65 @@
-// pages/cart.js
 import { useContext } from "react";
 import { CartContext } from "../src/app/services/cartContext";
 import { BackButton } from "../styles/products.styles";
 import Link from "next/link";
+import {
+  CartContainer,
+  CartItem,
+  CartImage,
+  CartDetails,
+  CartTitle,
+  TotalPrice,
+  QuantityCounter,
+} from "../styles/cart.styles";
 
 const Cart = () => {
   const { cartItems } = useContext(CartContext);
 
-  const totalPrice = cartItems.reduce(
-    (total, product) => total + product.price,
+  // Create an object to track quantity of each item
+  const cartSummary = cartItems.reduce((acc, product) => {
+    const existingProduct = acc.find((item) => item.id === product.id);
+    if (existingProduct) {
+      existingProduct.quantity += 1;
+    } else {
+      acc.push({ ...product, quantity: 1 });
+    }
+    return acc;
+  }, []);
+
+  const totalPrice = cartSummary.reduce(
+    (total, product) => total + product.price * product.quantity,
     0
   );
 
   return (
-    <div style={styles.cartContainer}>
+    <CartContainer>
       <BackButton>
         <Link href="/">🠔 Voltar para produtos</Link>
       </BackButton>
       <h1>Shopping Cart</h1>
-      {cartItems.length === 0 ? (
+      {cartSummary.length === 0 ? (
         <p>No items in cart.</p>
       ) : (
         <div>
-          {cartItems.map((item, index) => (
-            <div key={index} style={styles.cartItem}>
-              <img src={item.image} alt={item.title} style={styles.cartImage} />
-              <div>
-                <h2>
+          {cartSummary.map((item, index) => (
+            <CartItem key={index}>
+              <CartImage src={item.image} alt={item.title} />
+              <CartDetails>
+                <CartTitle>
                   {item.title.length > 65
-                    ? `${item.title.substring(0, 65)}`
+                    ? `${item.title.substring(0, 65)}...`
                     : item.title}
-                </h2>
-                <p>Price: ${item.price}</p>
-              </div>
-            </div>
+                </CartTitle>
+                <p>Price: ${item.price.toLocaleString()}</p>
+                <QuantityCounter>Quantity: {item.quantity}</QuantityCounter>
+              </CartDetails>
+            </CartItem>
           ))}
-          <h2>Total Price: ${totalPrice}</h2>
+          <TotalPrice>Total Price: ${totalPrice.toLocaleString()}</TotalPrice>
         </div>
       )}
-    </div>
+    </CartContainer>
   );
-};
-
-const styles = {
-  cartContainer: {
-    padding: "20px",
-  },
-  cartItem: {
-    display: "flex",
-    marginBottom: "20px",
-  },
-  cartImage: {
-    width: "100px",
-    marginRight: "20px",
-  },
 };
 
 export default Cart;
---
+++ styles/cart.styles.js
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+
+export const CartContainer = styled.div`
+  padding: 20px;
+  background-color: #f9f9ff;
+  min-height: 100vh;
+`;
+
+export const BackButton = styled.div`
+  font-size: 1rem;
+  font-weight: bold;
+  color: #7065f0;
+  margin-bottom: 20px;
+  cursor: pointer;
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export const CartItem = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: #fff;
+  border-radius: 8px;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+  padding: 20px;
+  margin-bottom: 20px;
+`;
+
+export const CartImage = styled.img`
+  width: 100px;
+  height: 100px;
+  object-fit: cover;
+  border-radius: 8px;
+  margin-right: 20px;
+`;
+
+export const CartDetails = styled.div`
+  display: flex;
+  font-weight: 600;
+  flex-direction: column;
+  justify-content: center;
+  font-family: "Plus Jakarta Sans", sans-serif;
+`;
+
+export const CartTitle = styled.h2`
+  font-size: 1rem;
+  font-weight: 600;
+  color: #080929;
+  margin-bottom: 10px;
+  font-family: "Plus Jakarta Sans", sans-serif;
+`;
+
+export const TotalPrice = styled.h2`
+  font-size: 1.25rem;
+  font-weight: bold;
+  color: #7065f0;
+  text-align: right;
+  margin-top: 20px;
+  font-family: "Plus Jakarta Sans", sans-serif;
+`;
+
+export const QuantityCounter = styled.p`
+  font-size: 1rem;
+  font-weight: bold;
+  color: #444;
+  margin-top: 10px;
+`;
